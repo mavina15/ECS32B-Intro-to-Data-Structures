b12121be24437629a78dfafc9022b2868682f01a
Mel Avina <melissaavina.ma@gmail.com>
2023-04-11 20:14:19 -0700

    Update problem_4.py

--- problem_4.py
+++ problem_4.py
@@ -5,21 +5,26 @@
 
 def tidBit(n):
     
+    price = float(n)
     month = 1
-    downPay = n*.10
+    downPay = price*.10
     annIntRate = .12
-    currentBal = n - downPay
-    monthlyPay = (n-downPay)*.05
+    currentBal = price - downPay
+    monthlyPay = (price-downPay)*.05
+    monthlyInt = currentBal*annIntRate/12
+    monthlyPrin = monthlyPay - monthlyInt
+    pay = monthlyPay + monthlyPrin
+    remainBal = currentBal - pay
     
-    print(n, downPay, currentBal, monthlyPay)
-    print("month ", "currentBal ", "monthlyPay ", "monthlyInt ", "monthlyPrin ", "remainBal ")
+    print('{:<7s} {:<20s} {:<20s} {:<20s} {:<15s} {:<20s}'.format('Month','Starting Balance', 'Interest to Pay', 'Principal to Pay','Payment','Ending Balance'))
     
-    while currentBal > 0:
+    while remainBal > 0:
             monthlyInt = currentBal*annIntRate/12
             monthlyPrin = monthlyPay - monthlyInt
-            remainBal = currentBal - monthlyPay
-            print(month, currentBal, monthlyPay, monthlyInt, monthlyPrin, remainBal)
+            pay = monthlyPay + monthlyPrin
+            remainBal = currentBal - pay
+            print('{:<7d} {:<20.2f} {:<20.2f} {:<20.2f} {:<15f} {:<20.2f}'.format(month, currentBal, monthlyInt, monthlyPrin, pay, remainBal))
+            currentBal = currentBal - pay
             month = month + 1
-            currentBal = currentBal - monthlyPay
         
 tidBit(5000)
