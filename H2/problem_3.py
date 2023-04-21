944f093ec7f57161b7fdbf2341cf77a1a3c7f627
Mel Avina <melissaavina.ma@gmail.com>
2023-04-20 21:19:54 -0700

    Create problem_3.py

---
+++ H2/problem_3.py
@@ -0,0 +1,25 @@
+# Mel Avina-Beltran
+# 4/20/23
+# H2 P3
+# Goal: Defines a function to reverse the elements in a list
+
+def reverse(lyst):
+  left = 0
+  right = len(lyst) - 1
+  while left < right:
+    swap(lyst, left, right) 
+    left += 1
+    right -= 1
+
+def swap(lyst, x, y):
+  lyst[x], lyst[y] = lyst[y], lyst[x]
+
+def main():
+  lyst = list(range(4)) 
+  reverse(lyst)
+  print(lyst)
+  lyst = list(range(3)) 
+  reverse(lyst)
+  print(lyst)
+
+if __name__ == "__main__": main()
