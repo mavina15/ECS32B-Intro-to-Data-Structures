45bf688ee2cf8c2ef62961b94821f5b4614c1f81
Mel Avina <melissaavina.ma@gmail.com>
2023-04-21 12:31:25 -0700

    Update problem_2.py

--- H2/problem_2.py
+++ H2/problem_2.py
@@ -1,22 +1,27 @@
 # Mel Avina-Beltran
 # 4/20/23
 # H2 P2
-# Goal: Optimizes linear search for sorted lists
+# Goal: Modified version of sequential search for sorted lists
 
-def linearSearch(target, lyst):
-  position = 0
-  while position < len(lyst):
-    if target == lyst[position]: 
-      return position
-    elif target < lyst[position]:
+def seqSearch(target, lyst):
+  i = 0
+  # completes n searches of position and length of lyst
+  # as position is less than the length of list 
+  while i < len(lyst):
+  # if the target is equal to the list position, returns the position
+    if target == lyst[i]: 
+      return i
+  # if the target is less than the list position, returns the position - 1 
+    elif target < lyst[i]:
       return -1
-    position += 1 
+  # else returns the position + 1 to satrt search over again
+    i += 1 
   return -1
 
+# prints sorted list
 def main():
-  print(linearSearch(3, [0, 1, 2, 3, 4])) 
-  print(linearSearch(3, [0, 1, 2]))
-  print(linearSearch(3, [0, 4, 5, 6]))
+  print(seqSearch(3, [0, 1, 2, 3, 4])) 
+  print(seqSearch(3, [0, 1, 2]))
+  print(seqSearch(3, [0, 4, 5, 6]))
 
 if __name__ == "__main__": main()
-
