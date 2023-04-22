358bd5f36e41bb5a1abc287c9c65e515ac4a363c
Mel Avina <melissaavina.ma@gmail.com>
2023-04-22 15:32:17 -0700

    Update problem_4.py

--- H2/problem_4.py
+++ H2/problem_4.py
@@ -4,7 +4,9 @@
 # Goal: Allows the user to specify the direction of a selection sort
 
 def selectionSort(lyst, reverse = False): 
+  # list is sorted in ascending order if reverse is false, else list is sorted in descending order
   if not reverse:
+    # here list is sorted in ascending order
     i=0
     while i < len(lyst) - 1: 
       minIndex = i 
@@ -17,6 +19,7 @@ def selectionSort(lyst, reverse = False):
         swap(lyst, minIndex, i)
       i += 1 
   else:
+    # here list is sorted in descending order
     i = len(lyst) - 1
     while i > 0: 
       minIndex = i 
@@ -30,9 +33,11 @@ def selectionSort(lyst, reverse = False):
       i -= 1
 
 def swap(lyst, x, y):
+  # exchanges elements at positions x and y
   lyst[x], lyst[y] = lyst[y], lyst[x]
-
+  
 def main():
+  # creates 4 list, sorted and printed 
   lyst = [2, 4, 3, 0, 1, 5] 
   selectionSort(lyst) 
   print(lyst)
