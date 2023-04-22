dc43bd4d2783910f30a17a4310fd284d717c83b9
Mel Avina <melissaavina.ma@gmail.com>
2023-04-22 15:07:17 -0700

    Update problem_3.py

--- H2/problem_3.py
+++ H2/problem_3.py
@@ -1,20 +1,25 @@
 # Mel Avina-Beltran
 # 4/20/23
 # H2 P3
-# Goal: Defines a function to reverse the elements in a list
+# Goal: Defines an efficient function that reverses the elements in a list
 
 def reverse(lyst):
+  # first element of list
   left = 0
+  # last element of list
   right = len(lyst) - 1
   while left < right:
     swap(lyst, left, right) 
+    # continues next iterations accordingly
     left += 1
     right -= 1
 
+# swap function exchanges positions of two items in list
 def swap(lyst, x, y):
   lyst[x], lyst[y] = lyst[y], lyst[x]
 
 def main():
+  # list range creates list of n numbers to reverse and print
   lyst = list(range(4)) 
   reverse(lyst)
   print(lyst)
